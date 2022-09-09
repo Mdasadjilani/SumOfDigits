fe0dd05547e01c4ee8f492a0f24db40ce064e4f7
Mdasadjilani <asadjilani20@gmail.com>
2022-09-09 19:45:03 +0530

    sumofdigits

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int number = Int32.Parse(Console.ReadLine());
+
+            int backupvalue = number;
+            int sum = 0;
+            int result = 0;
+            while (number != 0)
+            {
+                result = number % 10;
+                sum = sum + result;
+                number = number / 10;
+            }
+            Console.WriteLine("sum of digits {0} is {1}", backupvalue, sum);
+            Console.ReadLine();
+        }
+    }
+}
